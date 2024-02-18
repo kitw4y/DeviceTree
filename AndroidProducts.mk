4b4e8327af49c523141c30503ec38525af66b1e0
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-02-19 05:22:20 +0700

    bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/rising_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    rising_lancelot-user \
+    rising_lancelot-userdebug \
+    rising_lancelot-eng
